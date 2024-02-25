4ca87f527e247e28efa464132b54577a8b75acbd
root <root@DESKTOP-QF4L4KN>
2024-02-25 18:50:23 +0700

    Update file

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #1 Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1.
-
 echo -e "Building image item-app:v1!\n"
 docker build -t item-app:v1 .
 
@@ -18,22 +17,35 @@ echo -e "Please wait to next step...\n"
 sleep 3s
 
 #3 Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
-docker tag item-app:v1 indra26dev/item-app:v1
+# jika menggunakan docker hub sebagai repositroy
+# docker tag item-app:v1 indra26dev/item-app:v1
+
+# jika menggunakan  github packages
+docker tag item-app:v1 ghcr.io/indra-yana/item-app:v1
 
 # Jeda script!
 echo -e "Please wait to next step...\n"
 sleep 3s
 
 #4 Login ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat) via Terminal.
-echo -e "Login to dockerhub...\n"
-echo $PASSWORD_DOCKER_HUB | docker login -u indra26dev --password-stdin
+# jika menggunakan docker hub
+# echo -e "Login to dockerhub...\n"
+# echo $PASSWORD_DOCKER_HUB | docker login -u indra26dev --password-stdin
+
+# jika menggunakan github packages
+echo -e "Login to github packages...\n"
+echo $GHCR_PAT | docker login ghcr.io -u indra-yana --password-stdin
 
 # Jeda script!
 echo -e "Please wait to next step...\n"
 sleep 3s
 
 #5 Mengunggah image ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
-docker push indra26dev/item-app:v1
+# jika menggunakan docker hub
+# docker push indra26dev/item-app:v1
+
+# jika menggunakan github packages
+docker push ghcr.io/indra-yana/item-app:v1
 
 echo -e "Done! Exited...\n"
 sleep 2s
